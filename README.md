366785c8783da65ce03566c6c62891bba03794a8
OlegTemek <olegtemek@gmail.com>
2023-05-12 01:11:06 +0600

    first

---
+++ README.md
@@ -0,0 +1,37 @@
+# Ip range checker
+
+##  Features
+* You can add ips to file -> ips.txt
+* You can add ranges to file -> ranges.txt
+* You get ips match in your ranges
+
+## General settings
+
+1. Build project
+``` bash
+$ npm run build
+```
+2. Create 2 txt files
+``` bash
+$ touch ./build/ranges.txt && touch ./build/ips.txt
+```
+3. ranges.txt
+> 192.168.1.1, 193.168.1.1<br>
+> 200.167.1.1, 203.145.4.4
+
+4. ips.txt
+> 192.169.1.1,<br>
+> 192.170.1.1
+
+5.
+``` bash
+$ cd ./build && node index.js
+```
+
+<br />
+
+---
+
+➡️ [more projects...](https://github.com/olegtemek)
+
+---
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+
+function isIPInRange(ranges: string[][], ipsCheck: string[]) {
+  const ipToNum = (ip: string): number => {
+    //make array with numbers
+    let arrayNum: number[] = ip.split('.').map(i => parseInt(i))
+    //ip = decimal number
+    let result = arrayNum.reduce((prev: number, item: number, index: number) => {
+      return prev + item * Math.pow(256, 3 - index);
+    }, 0)
+    return result;
+  }
+
+  ranges.forEach(range => {
+    const ip1Num: number = ipToNum(range[0]);
+    const ip2Num: number = ipToNum(range[1]);
+
+    ipsCheck.forEach(ipCheck => {
+
+      let ipCheckNum: number = ipToNum(ipCheck);
+
+      if (ipCheckNum >= ip1Num && ipCheckNum <= ip2Num) {
+        console.log(`Ip match in this range: (${range[0]} - ${range[1]}) - ${ipCheck}`);
+      }
+    });
+  });
+}
+
+
+const rangesFile: string[][] = fs.readFileSync(`${process.env.PWD}/ranges.txt`, 'utf-8').toString().split(/\r?\n/).map(i => i.split(', '));
+const ipsFile: string[] = fs.readFileSync(`${process.env.PWD}/ips.txt`, 'utf-8').toString().split(/\r?\n/);
+isIPInRange(rangesFile, ipsFile)
+
